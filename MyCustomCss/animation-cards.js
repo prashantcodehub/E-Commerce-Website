16d33fc572b0168386404db699e48fe6320f6b38
unknown <pradip1232>
2023-11-20 07:19:59 -0500

    update the some segments

---
+++ MyCustomCss/animation-cards.js
@@ -0,0 +1,14 @@
+let section = document.querySelectorAll(".section-dom");
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top > offset + height) {
+            sec.classList.add("show-animate")
+        } else {
+            sec.classList.remove("show-animate");
+        }
+    })
+}
