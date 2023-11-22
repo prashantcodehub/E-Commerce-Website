6a4b857a79138932ae585c045dc4518ee0f25398
unknown <pradip1232>
2023-11-22 17:19:51 -0500

    update the some segments

--- My-Animations/right-animation.js
+++ My-Animations/right-animation.js
@@ -1,25 +1,19 @@
-// Trigger CSS animations on scroll.
-// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
-
-// Looking for a version that also reverses the animation when
-// elements scroll below the fold again?
-// --> Check https://codepen.io/bramus/pen/vKpjNP
 
 jQuery(function($) {
   
-    // Function which adds the 'animated' class to any '.animatable' in view
-    var doAnimations = function() {
+    
+    var doAnimations = function() {why-heading
       
-      // Calc current offset and get all animatables
+    
       var offset = $(window).scrollTop() + $(window).height(),
           $animatables = $('.animatable');
       
-      // Unbind scroll handler if we have no animatables
+  
       if ($animatables.length == 0) {
         $(window).off('scroll', doAnimations);
       }
       
-      // Check all animatables and animate them if necessary
+      
           $animatables.each(function(i) {
          var $animatable = $(this);
               if (($animatable.offset().top + $animatable.height() - 20) < offset) {
@@ -29,8 +23,23 @@ jQuery(function($) {
   
       };
     
-    // Hook doAnimations on scroll, and trigger a scroll
+   
       $(window).on('scroll', doAnimations);
     $(window).trigger('scroll');
   
   });
+
+
+  
+
+  var _window = window,Splitting = _window.Splitting,ScrollOut = _window.ScrollOut;
+Splitting();
+ScrollOut({
+  targets: '.word',
+  scrollingElement: '.page' });
+
+
+
+  
+
+  
--- MyCustomCss/animation-cards.js
+++ MyCustomCss/animation-cards.js
@@ -12,3 +12,15 @@ window.onscroll = () => {
         }
     })
 }
+
+
+
+var _window = window,
+    Splitting = _window.Splitting,
+    ScrollOut = _window.ScrollOut;
+Splitting();
+ScrollOut({
+    targets: '.word',
+    scrollingElement: '.page'
+});
+
