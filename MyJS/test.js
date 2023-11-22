0ea50c327e293090980ee7790af822689da99bad
unknown <pradip1232>
2023-11-21 19:33:41 -0500

    update the some segments

--- MyJS/test.js
+++ MyJS/test.js
@@ -16,3 +16,25 @@ $(document).ready(function () {
         $(this).text(i + 1).prepend("0");
     });
 });
+
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items: 1,
+        },
+        768:{
+            items: 2,
+        },
+        1100:{
+            items: 3,
+        },
+        1400:{
+            items: 4,
+            loop: false,
+        }
+    }
+});
