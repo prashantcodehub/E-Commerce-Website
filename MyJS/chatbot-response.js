5941cabd7e585de8bee4bf3b69693e40f1f7a009
unknown <pradip1232>
2024-01-03 18:38:02 -0500

    chnage somthing and resolve the spacing issues & put the animation

--- MyJS/chatbot-response.js
+++ MyJS/chatbot-response.js
@@ -54,7 +54,7 @@ const setScrollPosition = () => {
 const responseObj = {
     hello: "Hey ! How are you doing ?",
     hey: "Hey! What's Up",
-    today: new Date().toDateString(),
+    date: new Date().toDateString(),
     time: new Date().toLocaleTimeString(),
   };
   
